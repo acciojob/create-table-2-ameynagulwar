0d45318f911059047588e306756450932e6bd5ae
Amey <130839577+ameynagulwar@users.noreply.github.com>
2023-06-26 17:40:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
+// script.js
+
 function createTable() {
-    //Write your code here
-  
+  var rows = parseInt(prompt("Input number of rows"));
+  var columns = parseInt(prompt("Input number of columns"));
+
+  var table = document.getElementById("myTable");
+  table.innerHTML = '';
+
+  for (var i = 0; i < rows; i++) {
+    var row = document.createElement("tr");
+
+    for (var j = 0; j < columns; j++) {
+      var cell = document.createElement("td");
+      cell.textContent = "Row-" + i + " Column-" + j;
+      row.appendChild(cell);
+    }
+
+    table.appendChild(row);
+  }
 }
